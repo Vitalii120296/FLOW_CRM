395751bba5f8e95f5057387ee91c837767599f82
Vitalii <v.hulaievych@gmail.com>
2026-01-30 12:21:00 +0000

    add gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/b2b_crm/',
+  base: '/FLOW_CRM/',
   plugins: [
     react({
       babel: {
