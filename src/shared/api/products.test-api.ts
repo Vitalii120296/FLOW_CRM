91171ced0d9bcf137b0f81b4be7b601234b44887
Vitalii <v.hulaievych@gmail.com>
2026-02-08 16:18:57 +0000

    fixed pictures path

--- src/shared/api/products.test-api.ts
+++ src/shared/api/products.test-api.ts
@@ -1,52 +1,51 @@
 import type { Product } from '../../types/product'
-import DefaultImage from '/public/productImages/defaultProductImage.webp'
 
 const productCards: Product[] = [
   {
     id: '1',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'iphone',
     price: '239.99',
     description: 'black ',
   },
   {
     id: '2',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'title',
     price: '239',
     description: 'black swq swqs qws qw qwqsqwsq ws qws wq ',
   },
   {
     id: '3',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'iphone',
     price: '239',
     description: 'blacks wq wqs wqs qw ',
   },
   {
     id: '4',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'iphone',
     price: '239',
     description: 'black  swqs w w qw qws qwq ',
   },
   {
     id: '5',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'iphone',
     price: '239',
     description: 'black swq sq q',
   },
   {
     id: '6',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'iphone',
     price: '239',
     description: 'black ',
   },
   {
     id: '7',
-    image: DefaultImage,
+    image: `public/productImages/defaultProductImage.webp`,
     title: 'iphone',
     price: '239',
     description: 'black ',
