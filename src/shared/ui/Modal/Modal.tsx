c172dc1bbd2a5a8b0700577128ad2801372ba5a7
Vitalii <v.hulaievych@gmail.com>
2026-02-21 14:05:02 +0000

    animation modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import s from './Modal.module.scss'
 import { CloseButton } from '../CloseButton/CloseButton'
 import cn from 'classnames'
+import { motion, AnimatePresence } from 'framer-motion'
 
 type Props = {
   isOpen: boolean
@@ -22,25 +23,37 @@ export const Modal: React.FC<Props> = ({ isOpen, onClose, title, titleId, childr
     }
   }, [isOpen])
 
-  if (!isOpen) return null
-
   return (
-    <div
-      role="dialog"
-      aria-modal="true"
-      aria-labelledby={titleId}
-      className={s.modal}
-      onClick={onClose}
-    >
-      <div className={s.modal__content} onClick={(e) => e.stopPropagation()}>
-        <div className={s.modal_title}>
-          <h2 className={cn('h2', s.textTitle)} id={titleId}>
-            {title}
-          </h2>
-          <CloseButton onClick={onClose} ariaLabel="Close modal" />
+    <>
+      {isOpen && (
+        <div
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby={titleId}
+          className={s.modal}
+          onClick={onClose}
+        >
+          <motion.div
+            className={s.modal__content}
+            onClick={(e) => e.stopPropagation()}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.5 }}
+            transition={{
+              duration: 0.2,
+              ease: 'easeIn',
+            }}
+          >
+            <div className={s.modal_title}>
+              <h2 className={cn('h2', s.textTitle)} id={titleId}>
+                {title}
+              </h2>
+              <CloseButton onClick={onClose} ariaLabel="Close modal" />
+            </div>
+            {children}
+          </motion.div>
         </div>
-        {children}
-      </div>
-    </div>
+      )}
+    </>
   )
 }
