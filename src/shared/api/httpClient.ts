fc93b1d9c4a899d57d7d3d7b477c2a125cdcc8ce
Vitalii <v.hulaievych@gmail.com>
2026-03-05 13:25:34 +0000

    add interceptor for refresh

--- src/shared/api/httpClient.ts
+++ src/shared/api/httpClient.ts
@@ -1,31 +1,32 @@
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
 import { authService } from '../../services/authService'
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 })
-// httpClient.interceptors.request.use((request) => {
-//   const accessToken = localStorage.getItem('accessToken')
-
-//   if (accessToken) {
-//     request.headers.Authorization = `Bearer ${accessToken}`
-//   }
-
-//   return request
-// })
 
 httpClient.interceptors.response.use(
   (res) => res.data,
-  (error: AxiosError) => {
+  async (error: AxiosError) => {
     const status = error.response?.status
+    const originalRequest = error.config as (AxiosRequestConfig & { _retry?: boolean }) | undefined
+
+    if (status === 401 && originalRequest && !originalRequest._retry) {
+      originalRequest._retry = true
+
+      try {
+        await authService.refresh()
+        // повторюємо оригінальний запит
+        return httpClient(originalRequest)
+      } catch (refreshError) {
+        return Promise.reject(refreshError)
+      }
+    }
 
     switch (status) {
       case 400:
         throw new Error('Bad Request')
-      case 401:
-        authService.refresh()
-        throw new Error('Unauthorized')
       case 403:
         throw new Error('Forbidden')
       case 404:
